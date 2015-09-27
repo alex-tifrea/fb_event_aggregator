30b4c5fecb51d13465fe6350fda9bdd19c2d4cb4
Mihai Varga <mihai.mv13@gmail.com>
2015-09-27 13:48:32 +0200

    client: load a map

--- client/fb-events.js
+++ client/fb-events.js
@@ -1,1 +1,10 @@
-console.log('it works');
+var map;
+function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {
+            lat: 44.4297378,
+            lng: 26.1055353
+        },
+        zoom: 14
+    });
+}
